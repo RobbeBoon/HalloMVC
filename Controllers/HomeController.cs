a348a8c8e96cb9e2df8099e91f70de3253eb192d
Robbe Boon <robbe.boon@student.hogent.be>
2020-10-15 11:30:42 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HalloMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About(int id)
+        {
+            return Content($"Hello from About{id}");
+        }
+
+        public IActionResult Contact()
+        {
+            return Content("Hello from Contact");
+        }
+    }
+}
